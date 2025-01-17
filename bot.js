e5baaf8e79e4c92d780f2cd89e2d2a86524a037a
hariton <hariton@mail.ru>
2025-01-17 13:32:43 +0300

    123

--- bot.js
+++ bot.js
@@ -1,19 +1,68 @@
-const { Bot} = require('grammy');
+const { Bot, InlineKeyboard} = require('grammy');
 
 const bot = new Bot('7744251076:AAEvkaRlygAhQFE2IYTL4mkCwGfmLLte9TE');
 
+
+//старт
 bot.command('start',(ctx) => {
-    ctx.reply('Прив ку чикса! я саня. /help чтобы узнать что я умею.');
+    const keyboard = new InlineKeyboard()
+    .text('Орел и решка', 'option1')
+    .text('Камень, ножница, бумага', 'option2')
+    .text('Угадай число', 'option3')
+    .row()
+    .text('помощь', 'help');
+    
+    ctx.reply('Привет я саня шляпик, если ты еще тут, то посмотри во что я умею играть:', {reply_markup: keyboard});
 });
 
+//обработка кнопок
+bot.on('callback_query:data', (ctx) => {
+    const data = ctx.callbackQuery.data;
+    const keyboard = new InlineKeyboard();
+    if ( data === 'option1') {
+        
+        ctx.answerCallbackQuery( );
+        ctx.reply('Сыграем в орел и решку, выбери сторону:', {reply_markup: keyboard});
+        if ( data === 'reshka'){
+
+        }
+    } 
+    else if ( data === 'option2') {
+        ctx.answerCallbackQuery('Запуск опции 2.');
+        ctx.reply('Содержание опции 2.');
+    } 
+    else if ( data === 'option 2') {
+        ctx.answerCallbackQuery('Запуск опции 2.');
+        ctx.reply('Содержание опции 2.');
+    } 
+    else if (data === 'help') {
+        ctx.answerCallbackQuery('Выберите опцию для получения информации.');
+        ctx.reply('Для получения информации выберите опцию.');
+    } 
+    else {
+        ctx.reply('Неверная команда');
+    }
+});
+
+
+
+
+
+function orelireshka(ctx) {
+
+}
+//помощь
 bot.command('help',(ctx) => {
     ctx.reply('/shlyapa - привет шляпик\n/echo - передразнить тебя\n/jonkler - щутка :)');
 });
 
+//повторить сообщение
 bot.command('echo',(ctx) => {
-    const message = ctx.message.text.split('').slice(1).join('');
-    ctx.reply(message || 'введи сообщение для пво');
+    const message = ctx.message.text.split(' ').slice(1).join(' ');
+    ctx.reply(message || 'введи сообщение для пива');
 });
+
+//шутка
 bot.command('jonkler', async (ctx) => {
     const jokes = [
         'Купаются как-то два тимы в бассейне',
