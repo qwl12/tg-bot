60fe40a1353df714918a54a6802d6da86d2f4fc2
hariton <hariton@mail.ru>
2025-01-17 14:58:50 +0300

    flipcoin added

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const { Bot, InlineKeyboard} = require('grammy');
+const { Bot, InlineKeyboard } = require('grammy');
 
 const bot = new Bot('7744251076:AAEvkaRlygAhQFE2IYTL4mkCwGfmLLte9TE');
 
@@ -6,7 +6,7 @@ const bot = new Bot('7744251076:AAEvkaRlygAhQFE2IYTL4mkCwGfmLLte9TE');
 //старт
 bot.command('start',(ctx) => {
     const keyboard = new InlineKeyboard()
-    .text('Орел и решка', 'option1')
+    .text('Орел и решка', 'flipcoin')
     .text('Камень, ножница, бумага', 'option2')
     .text('Угадай число', 'option3')
     .row()
@@ -15,42 +15,45 @@ bot.command('start',(ctx) => {
     ctx.reply('Привет я саня шляпик, если ты еще тут, то посмотри во что я умею играть:', {reply_markup: keyboard});
 });
 
+bot.command('flipcoin', (ctx) =>{
+   const keyboard = new InlineKeyboard().text('Решка', 'reshka').text('Орел', 'orel');
+   ctx.reply('Выбери орел или решка', {reply_markup: keyboard});
+})
+
 //обработка кнопок
-bot.on('callback_query:data', (ctx) => {
-    const data = ctx.callbackQuery.data;
-    const keyboard = new InlineKeyboard();
-    if ( data === 'option1') {
-        
-        ctx.answerCallbackQuery( );
-        ctx.reply('Сыграем в орел и решку, выбери сторону:', {reply_markup: keyboard});
-        if ( data === 'reshka'){
+bot.on('callback_query:data', async (ctx) => {
+    
+    const data = ctx.callbackQuery.data;    
 
+        if( data === 'reshka' || data === 'orel') {
+            const result = Math.random() < 0.5 ? 'reshka' : 'orel';
+            const userChoice = data === 'orel' ? 'Орел' : 'Решка';
+            const botResult = result === 'orel' ? 'Орел' : 'Решка';
+               
+            const resultMessage = userChoice === botResult
+            ? `ты выбрал ${userChoice}, и угадал(а) Сыграем еще?`
+            :  `ты выбрал ${userChoice}, но выпал ${botResult}, Сыграем еще?`;
+
+
+            const playAgainKeyboard = new InlineKeyboard()
+            .text('да', 'flipcoin')
+            .text('нет', 'start');
+
+            await ctx.reply(resultMessage, {reply_markup: playAgainKeyboard});
+        } else if (data === 'flipcoin') {
+            ctx.reply('Сыграем в орел и решку? /flipcoin')
+        } else if (data === 'start') {
+            ctx.reply ('Перейдем в начало. /start')
         }
-    } 
-    else if ( data === 'option2') {
-        ctx.answerCallbackQuery('Запуск опции 2.');
-        ctx.reply('Содержание опции 2.');
-    } 
-    else if ( data === 'option 2') {
-        ctx.answerCallbackQuery('Запуск опции 2.');
-        ctx.reply('Содержание опции 2.');
-    } 
-    else if (data === 'help') {
-        ctx.answerCallbackQuery('Выберите опцию для получения информации.');
-        ctx.reply('Для получения информации выберите опцию.');
-    } 
-    else {
-        ctx.reply('Неверная команда');
-    }
-});
+
+
+    });
 
 
 
 
 
-function orelireshka(ctx) {
 
-}
 //помощь
 bot.command('help',(ctx) => {
     ctx.reply('/shlyapa - привет шляпик\n/echo - передразнить тебя\n/jonkler - щутка :)');
